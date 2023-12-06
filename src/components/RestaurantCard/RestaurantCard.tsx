4bc9467ece7e07b58ac87e94e971a2c3eb37c433
Jono Kolnik <1164060+JonathanKolnik@users.noreply.github.com>
2023-12-06 13:36:37 -0500

    s

--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -109,7 +109,7 @@ const Description = styled(Body)`
 const StyledBadge = styled(Badge)`
   margin-top: 1.5rem;
   margin-right: 0.5rem;
-  background: ${({ theme: { color } }) => color.buttonPrimary};
+  background: ${({ theme: { color } }) => color.buttonSecondary};
 `
 
 const StyledHeading = styled(Heading)(
